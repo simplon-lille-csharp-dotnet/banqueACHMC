be94675b2cc2d803b73dc7a89f00d1a28233da83
MarsaColombo <timbiaou@gmail.com>
2023-12-06 23:11:57 +0100

    docs: :memo: Added documentation for every of parts of the program

--- ConsoleApp2/views/ICompteEpargneUI.cs
+++ ConsoleApp2/views/ICompteEpargneUI.cs
@@ -3,6 +3,9 @@ namespace ConsoleApp2
 {
     public interface ICompteEpargneUI 
     {
+        /// <summary>
+        /// Displays the menu for the Compte Epargne (Savings Account).
+        /// </summary>
         void AfficherMenuCompteEpargne();
     }
 }
