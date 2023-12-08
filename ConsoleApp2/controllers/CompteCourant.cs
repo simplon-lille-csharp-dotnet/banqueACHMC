bc2bff76154f509336598476a2a6a023a4ef27c8
ACH90 <chassagne.alexandre@gmail.com>
2023-12-08 12:01:30 +0100

    Feat : CompteCourant.cs

--- ConsoleApp2/controllers/CompteCourant.cs
+++ ConsoleApp2/controllers/CompteCourant.cs
@@ -1,8 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp2;
+using System.Reflection.Metadata;
 
 internal class CompteCourant : CompteBancaire
 {
+
+  public decimal montant { get; set; }
+    public virtual bool AjouterArgent(decimal montant)
+    { 
+        if (montant > 1000)
+        {
+            //throw new InvalidOperationException("Montant trop élevé");
+            Console.WriteLine("Montant trop élevé");
+            return false;
+         
+        }
+        return true;
+    
+    }
+
+    public override bool RetirerArgent(decimal montant)
+    {
+        if (montant > 500)
+        {
+            //throw new InvalidOperationException("Montant trop élevé");
+            Console.WriteLine("Montant trop élevé");
+            return false;
+        }
+        return true;
+    }
+
     public void InteretCourant()
     {
         throw new System.NotImplementedException();
