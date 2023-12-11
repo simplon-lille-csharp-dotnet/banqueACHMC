8dc63b08793a760f21153a6b0ef1c213a7a39d93
MarsaColombo <timbiaou@gmail.com>
2023-12-11 14:41:06 +0100

    test: :white_check_mark: Creating test to add and withdraw money
    Changed the if statements for all the exeption that the code can catch, in the form of if and else if
    
    Added Test Unit project in the same namespace, add the reference in the csproj, and modified the methods of comptebancaire

--- ConsoleApp2/controllers/CompteEpargne.cs
+++ ConsoleApp2/controllers/CompteEpargne.cs
@@ -1,123 +1,83 @@
-﻿
-namespace ConsoleApp2;
+﻿using System;
 
-/// <summary>
-/// Represents a savings account.
-/// </summary>
-/// <remarks>
-/// This class inherits from the <see cref="CompteBancaire"/> class and adds additional properties and methods specific to a savings account.
-/// </remarks>
-public class CompteEpargne : CompteBancaire
+namespace ConsoleApp2
 {
-   
-
- 
-
-<<<<<<< HEAD
-    public decimal tauxInteret;
-    public int nombreRetraitsAutorises;
-
-
-    /// <summary>
-    /// Gets or sets the number of years.
-    /// </summary>
-    public int NombreAnnees { get; set; }
-    /// <summary>
-    /// Gets or sets the estimated balance of the savings account.
-    /// </summary>
-    public decimal SoldeEstime { get; set; }
-    /// <summary>
-    /// Gets or sets the interest rate for the savings account.
-    /// </summary>
-    public decimal TauxInteret { get; set; }
-
     /// <summary>
     /// Represents a savings account.
-    /// 
     /// </summary>
-    /// <param name="tauxInteret">The interest rate for the savings account.</param>
-    /// <param name="nombreRetraitsAutorises">The number of allowed withdrawals for the savings account.</param>
-<<<<<<< HEAD
-=======
-    public CompteEpargne()
+    /// <remarks>
+    /// This class inherits from the <see cref="CompteBancaire"/> class and adds additional properties and methods specific to a savings account.
+    /// </remarks>
+    public class CompteEpargne : CompteBancaire
     {
-     
-    }
->>>>>>> 4164001c224d835e0e3f013bf6c7f8643a11d0b1
+        public new decimal SoldeEstime { get; set; }
+        public new decimal TauxInteret { get; set; }
+        public new int NombreRetraitsAutorises { get; set; }
 
-    /// <summary>
-    /// Calculates and updates the estimated balance of the savings account with the interest earned.
-    /// </summary>
-    /// <param name="solde">The current balance of the savings account.</param>
-    public void InteretEpargne(decimal solde)
-    {
-        SoldeEstime = solde;
-        decimal interets = SoldeEstime * TauxInteret;
-        SoldeEstime += interets;
-
-        //Console.WriteLine($"Intérêts : {interets} €");
-    }
-
-    /// <summary>
-    /// Affiche les prévisions pour les années spécifiées avec un intervalle spécifié
-    /// </summary>
-    /// <param name="nombreAnnees"></param>
-    /// <param name="intervalleAnnees"></param>
-    public void VoirPrevisions(int nombreAnnees, int intervalleAnnees)
-    {
-        Console.WriteLine($"Prévisions pour les {nombreAnnees} prochaines années avec un intervalle de {intervalleAnnees} ans:");
-        dateYear = DateTime.Now.Year;
-
-        Console.WriteLine($"Année de début : {dateYear}");
-        SoldeEstime = Solde;
-        for (int annee = 1; annee <= nombreAnnees; annee++)
+        /// <summary>
+        /// Represents a savings account.
+        /// </summary>
+        public CompteEpargne()
         {
-            InteretEpargne(SoldeEstime);
-            dateYear++;
+            // Initialize properties in the constructor
+            SoldeEstime = 0;
+            TauxInteret = 0;
+            NombreRetraitsAutorises = 0;
+        }
 
-            // Vérifiez si l'année est un multiple de l'intervalle spécifié
-            if (annee % intervalleAnnees != 0)
+        /// <summary>
+        /// Calculates and updates the estimated balance of the savings account with the interest earned.
+        /// </summary>
+        /// <param name="solde">The current balance of the savings account.</param>
+        public new void InteretEpargne(decimal solde)
+        {
+            if ( SoldeEstime == 0)
             {
-                continue;
+                // Handle the null case or throw an exception based on your requirements
+                // Example: throw new InvalidOperationException("SoldeEstime is not initialized.");
             }
 
-            Console.WriteLine($"Année {dateYear} : {SoldeEstime} €");
+            decimal interets = SoldeEstime * TauxInteret;
+            SoldeEstime += interets;
 
-
+            //Console.WriteLine($"Intérêts : {interets} €");
         }
-    }
-    /// <summary>
-    /// Définit le nombre de retraits autorisés pour le compte épargne.
-    /// </summary>
-    /// <param name="nombreRetraits">Le nombre de retraits autorisés.</param>
-    public void DefinirNombreRetraitsAutorises(int nombreRetraits)
-    {
-        NombreRetraitsAutorises = nombreRetraits;
-        Console.WriteLine($"Nombre de retraits autorisés défini à : {nombreRetraits}");
-    }
-=======
->>>>>>> 6fbf9b5ee5325cf0e2177eb38f6465e8c0960b29
-    public override bool RetirerArgent(decimal montant)
-    {
-        if (NombreRetraitsAutorises > 0)
+
+        /// <summary>
+        /// Définit le nombre de retraits autorisés pour le compte épargne.
+        /// </summary>
+        /// <param name="nombreRetraits">Le nombre de retraits autorisés.</param>
+        public new void DefinirNombreRetraitsAutorises(int nombreRetraits)
         {
-            NombreRetraitsAutorises--;
-            return base.RetirerArgent(montant);
+            if (nombreRetraits >= 0)
+            {
+                NombreRetraitsAutorises = nombreRetraits;
+                Console.WriteLine($"Nombre de retraits autorisés défini à : {nombreRetraits}");
+            }
+            else
+            {
+                // Handle the case where the input is invalid (negative)
+                // Example: throw new ArgumentException("Nombre de retraits autorisés ne peut pas être négatif.");
+            }
         }
-        else
-        {
-            Console.WriteLine("Nombre de retraits autorisés dépassé.");
-            return false;
-        }
-    }
 
-    /// <summary>
-    /// Gets the number of authorized withdrawals.
-    /// </summary>
-    /// <returns>The number of authorized withdrawals.</returns>
-    internal int GetNombreRetraitsAutorises()
-    {
-        return NombreRetraitsAutorises;
+        public override bool RetirerArgent(decimal montant)
+        {
+            if (NombreRetraitsAutorises > 0)
+            {
+                NombreRetraitsAutorises--;
+                return base.RetirerArgent(montant);
+            }
+            else
+            {
+                Console.WriteLine("Nombre de retraits autorisés dépassé.");
+                return false;
+            }
+        }
+
+        internal int GetNombreRetraitsAutorises()
+        {
+            return NombreRetraitsAutorises;
+        }
     }
 }
-
---
+++ TestUnit/TestTransictionCompteBancaire.cs
@@ -0,0 +1,200 @@
+﻿using ConsoleApp2;
+
+namespace TestUnit;
+
+[TestClass]
+public class TestTransictionCompteBancaire
+{
+    // Ajouter argent
+    [TestMethod]
+    public void Call_3_times_AjouterArgent_make_3_transactions()
+    {
+        // Arrange
+        CompteBancaire CompteBancaireToTest = new CompteBancaire();
+        CompteBancaireToTest.AjouterArgent(100);
+        CompteBancaireToTest.AjouterArgent(200);
+        CompteBancaireToTest.AjouterArgent(300);
+
+        // Act
+        List<Transaction> transactions = CompteBancaireToTest.TransactionList;
+
+        // Assert
+        Assert.AreEqual(3, transactions.Count);
+        Assert.AreEqual("Dépôt", transactions[0].Type);
+        Assert.AreEqual(100, transactions[0].Montant);
+        Assert.AreEqual("Dépôt", transactions[1].Type);
+        Assert.AreEqual(200, transactions[1].Montant);
+        Assert.AreEqual("Dépôt", transactions[2].Type);
+        Assert.AreEqual(300, transactions[2].Montant);
+
+    }
+
+    [TestMethod]
+    public void Call_AjouterArgent_with_negative_amount_return_false()
+    {
+        // Arrange
+        CompteBancaire CompteBancaireToTest = new CompteBancaire();
+
+        // Act
+        bool result = CompteBancaireToTest.AjouterArgent(-100);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void Call_AjouterArgent_with_positive_amount_return_true()
+    {
+        // Arrange
+        CompteBancaire CompteBancaireToTest = new CompteBancaire();
+
+        // Act
+        bool result = CompteBancaireToTest.AjouterArgent(100);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void Call_AjouterArgent_with_positive_amount_add_amount_to_Solde()
+    {
+        // Arrange
+        CompteBancaire CompteBancaireToTest = new CompteBancaire();
+        decimal amount = 100;
+
+        // Act
+        CompteBancaireToTest.AjouterArgent(amount);
+
+        // Assert
+        Assert.AreEqual(amount, CompteBancaireToTest.Solde);
+    }
+
+    [TestMethod]
+    public void Adding_money_with_more_than_2_decimal_places()
+    {
+        // Arrange
+        CompteBancaire compteBancaireToTest = new CompteBancaire();
+        decimal initialBalance = compteBancaireToTest.Solde;
+        int initialTransactionCount = compteBancaireToTest.TransactionList.Count;
+
+        // Act
+        bool result = compteBancaireToTest.AjouterArgent(100.123m);
+
+        // Assert
+        Assert.IsFalse(result);
+        Assert.AreEqual(initialBalance, compteBancaireToTest.Solde);
+        Assert.AreEqual(initialTransactionCount, compteBancaireToTest.TransactionList.Count);
+    }
+
+
+    // Retirer argent
+    [TestMethod]
+    public void Call_RetirerArgent_with_negative_amount_return_false()
+    {
+        // Arrange
+        CompteBancaire CompteBancaireToTest = new CompteBancaire();
+
+        // Act
+        bool result = CompteBancaireToTest.RetirerArgent(-100);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void Call_RetirerArgent_with_positive_amount_return_true()
+    {
+        // Arrange
+        CompteBancaire CompteBancaireToTest = new CompteBancaire();
+
+        // Solde
+        CompteBancaireToTest.AjouterArgent(100);
+        // Act
+        bool result = CompteBancaireToTest.RetirerArgent(100);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+    [TestMethod]
+    public void test_add_zero_amount()
+    {
+        // Arrange
+        CompteBancaire compte = new CompteBancaire();
+        decimal initialBalance = compte.Solde;
+        decimal amount = 0;
+
+        // Act
+        bool result = compte.AjouterArgent(amount);
+
+        // Assert
+        Assert.IsFalse(result);
+        Assert.AreEqual(initialBalance, compte.Solde);
+        Assert.AreEqual(0, compte.TransactionList.Count);
+    }
+    [TestMethod]
+    public void test_add_negative_amount()
+    {
+        // Arrange
+        CompteBancaire compte = new CompteBancaire();
+        decimal initialBalance = compte.Solde;
+        decimal amount = -100;
+
+        // Act
+        bool result = compte.AjouterArgent(amount);
+
+        // Assert
+        Assert.IsFalse(result);
+        Assert.AreEqual(initialBalance, compte.Solde);
+        Assert.AreEqual(0, compte.TransactionList.Count);
+    }
+    [TestMethod]
+    public void test_add_amount_with_more_than_2_decimal_places()
+    {
+        // Arrange
+        CompteBancaire compte = new CompteBancaire();
+        decimal initialBalance = compte.Solde;
+        decimal amount = 10.123M;
+
+        // Act
+        bool result = compte.AjouterArgent(amount);
+
+        // Assert
+        Assert.IsFalse(result);
+        Assert.AreEqual(initialBalance, compte.Solde);
+        Assert.AreEqual(0, compte.TransactionList.Count);
+    }
+    [TestMethod]
+    public void test_view_balance()
+    {
+        // Arrange
+        CompteBancaire compte = new CompteBancaire();
+        decimal initialBalance = 1000;
+        compte.Solde = initialBalance;
+
+        // Act
+        decimal balance = compte.VoirSolde();
+
+        // Assert
+        Assert.AreEqual(initialBalance, balance);
+    }
+    // Withdrawing 0 amount does not change balance and does not add transaction to history
+    
+    [TestMethod]
+    public void test_withdraw_zero_amount()
+    {
+        // Arrange
+        CompteBancaire compte = new CompteBancaire();
+        decimal initialBalance = 500;
+        compte.Solde = initialBalance;
+        decimal amount = 0;
+
+        // Act
+        bool result = compte.RetirerArgent(amount);
+
+        // Assert
+        Assert.IsFalse(result);
+        Assert.AreEqual(initialBalance, compte.Solde);
+        Assert.AreEqual(0, compte.TransactionList.Count);
+    }
+
+}
